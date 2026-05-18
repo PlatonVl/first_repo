5e4d0d3c520407cc31fc22cb32ad612eedd355cc
PlatonVl <vlasukplaton@gmail.com>
2026-05-18 21:57:25 +0300

    Add files via upload

---
+++ Homework_2_modul_4.py
@@ -0,0 +1,29 @@
+import os
+print(os.getcwd())
+
+
+def get_cats_info(path):
+    cats = []
+
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            for line in file:
+                cat_id, name, age = line.strip().split(",")
+
+                cats.append({
+                    "id": cat_id,
+                    "name": name,
+                    "age": age
+                })
+
+        return cats
+
+    except FileNotFoundError:
+        print("Файл не знайдено.")
+        return []
+
+
+cats = get_cats_info("cats.txt")
+
+for cat in cats:
+    print(cat)
